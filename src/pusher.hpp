a9e5c5c283be514d544568c3f44ab0c2c68e8bf1
Debian <debian@c4-10.lal.in2p3.fr>
2025-09-26 09:15:38 +0000

    Boris pusher

--- src/pusher.hpp
+++ src/pusher.hpp
@@ -47,6 +47,85 @@ public:
         for (auto& particle : particles)
         {
             // TODO implement the Boris pusher
+            
+            double x;
+            double x_half;
+            
+            double tx;
+            double ty;
+            double tz;
+            
+            double sx;
+            double sy;
+            double sz;
+            
+            double vx;
+            double vx_minus;
+            double vx_prime;
+            double vx_plus;
+            
+            double vy;
+            double vy_minus;
+            double vy_prime;
+            double vy_plus;
+            
+            double vz;
+            double vz_minus;
+            double vz_prime;
+            double vz_plus;
+
+
+            x = particle.position[0];
+            vx = particle.v[0];
+            vy = particle.v[1];
+            vz = particle.v[2];
+
+            x_half = x + vx*this->dt_/2;
+
+            double const iCell_float = x_half / this->layout_->cell_size(Direction::X)
+                                   + this->layout_->dual_dom_start(Direction::X);
+            int const iCell = static_cast<int>(iCell_float);
+            double reminder = iCell_float - iCell;
+
+            double Ex = interpolate(E.x, iCell, reminder);
+            double Ey = interpolate(E.y, iCell, reminder);
+            double Ez = interpolate(E.z, iCell, reminder);
+            double Bx = interpolate(B.x, iCell, reminder);
+            double By = interpolate(B.y, iCell, reminder);
+            double Bz = interpolate(B.z, iCell, reminder);
+
+            vx_minus = vx + particle.charge*this->dt_*Ex/(2*particle.mass);
+            vy_minus = vy + particle.charge*this->dt_*Ey/(2*particle.mass);
+            vz_minus = vz + particle.charge*this->dt_*Ez/(2*particle.mass);
+
+            tx = particle.charge*this->dt_*Bx/(2*particle.mass);
+            ty = particle.charge*this->dt_*By/(2*particle.mass);
+            tz = particle.charge*this->dt_*Bz/(2*particle.mass);
+
+            vx_prime = vx_minus + vy_minus*tz - vz_minus*ty;
+            vy_prime = vy_minus + vz_minus*tx - vx_minus*tz;
+            vz_prime = vz_minus + vx_minus*ty - vy_minus*tx;
+
+            sx = 2*tx/(1+(tx*tx +ty*ty + tz*tz));
+            sy = 2*ty/(1+(tx*tx +ty*ty + tz*tz));
+            sz = 2*tz/(1+(tx*tx +ty*ty + tz*tz));
+
+            vx_plus = vx_minus + vy_prime*sz - vz_prime*sy;
+            vy_plus = vy_minus + vz_prime*sx - vx_prime*sz;
+            vz_plus = vz_minus + vx_prime*sy - vy_prime*sx;
+
+            vx = vx_plus + particle.charge*this->dt_*Ex/(2*particle.mass);
+            vy = vy_plus + particle.charge*this->dt_*Ey/(2*particle.mass);
+            vz = vz_plus + particle.charge*this->dt_*Ez/(2*particle.mass);
+
+            x = x_half + vx*this->dt_/2;
+
+            particle.position[0] = x;
+            particle.v[0] = vx;
+            particle.v[1] = vy;
+            particle.v[2] = vz;
+            
+            
         }
     }
 
